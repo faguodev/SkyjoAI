1294bc9f692dc35a8966ea9dcfc5d90cad423e10
henry <henry.forman@hotmail.com>
2024-12-14 14:46:01 +0100

    PPO works baby. sample logs are also available.

--- rllib.py
+++ rllib.py
@@ -1,3 +1,5 @@
+import os
+
 from ray.tune.registry import register_env
 from ray.rllib.env.wrappers.pettingzoo_env import PettingZooEnv
 from ray.rllib.algorithms.ppo import PPOConfig
@@ -18,9 +20,10 @@ logger.setLevel(logging.INFO)
 class RewardDecayCallback(DefaultCallbacks):
     def on_train_result(self, *, algorithm, result, **kwargs):
         # Decay the reward scaling factor over training iterations
-        action_reward_decay = max(0.05, 1.0 - result["training_iteration"] * 0.01)
-        env = algorithm.workers.local_worker().env
-        env.update_action_reward_decay(action_reward_decay)
+        action_reward_decay = max(0.05, 1.0 - result["training_iteration"] * 0.005)
+        # env = algorithm.workers.local_worker().env
+        # env = algorithm.workers.local_env_runner.env
+        algorithm.config.env_config["action_reward_decay"] = action_reward_decay
         logger.info(action_reward_decay)
 
 
@@ -32,6 +35,7 @@ skyjo_config = {
     "reward_refunded": 10,
     "final_reward": 100,
     "score_per_unknown": 5.0,
+    "action_reward_decay": 1.0,
     "render_mode": "human",
 }
 
@@ -48,27 +52,27 @@ test_env = env_creator(skyjo_config)
 obs_space = test_env.observation_space
 act_space = test_env.action_space
 
-def policy_mapping_fn(agent_id, _):
-    return agent_id
+def policy_mapping_fn(agent_id, _, **kwargs):
+    print(agent_id)
+    return "policy_" + str(agent_id) #int(agent_id.split("_")[-1])
 
 config = (
     PPOConfig()
-    .training(model=model_config)
+    .training(model=model_config, )
     .environment("skyjo", env_config=skyjo_config)
     .framework('torch')
-    .training(model=model_config)
-    #.callbacks(RewardDecayCallback)
-    .env_runners(
-        num_env_runners=1,
+    .callbacks(RewardDecayCallback)
+    .env_runners(num_env_runners=1)
+    # .rollouts(num_rollout_workers=6)
+    .resources(num_gpus=1)
+    .multi_agent(
+        policies={
+            "policy_0": (None, obs_space[0], act_space[0], {"entropy_coeff":0.01}),
+            "policy_1": (None, obs_space[1], act_space[1], {"entropy_coeff":0.05}),
+            "policy_2": (None, obs_space[2], act_space[2], {"entropy_coeff":0.1})
+        },
+        policy_mapping_fn=policy_mapping_fn,#(lambda agent_id, *args, **kwargs: agent_id),
     )
-    # .multi_agent(
-    #     policies={
-    #         "player_0": (None, obs_space, act_space, {}),
-    #         "player_1": (None, obs_space, act_space, {}),
-    #         "player_2": (None, obs_space, act_space, {})
-    #     },
-    #     policy_mapping_fn=(lambda agent_id, *args, **kwargs: agent_id),
-    # )
     .evaluation(evaluation_num_env_runners=0)
     .debugging(log_level="INFO")
 )
@@ -86,6 +90,10 @@ def convert_to_serializable(obj):
         return obj.tolist()
     raise TypeError(f"Object of type {type(obj)} is not JSON serializable")
 
+
+if not os.path.exists("logs3"):
+    os.mkdir("logs3")
+
 for i in range(1000):
     result = algo.train()
     result.pop("config")
@@ -93,5 +101,5 @@ for i in range(1000):
 
     if i % 5 == 0:
         # Save result to JSON file
-        with open(f"result_iteration_{i}.json", "w") as f:
+        with open(f"logs3/result_iteration_{i}.json", "w") as f:
             json.dump(result, f, indent=4, default=convert_to_serializable)
